eb4f1c22be04185b8fc507731452692e28d6bb59
Chathan Driehuys <cdriehuys@gmail.com>
2016-08-07 12:28:47 -0400

    Setting up django-nose as a test runner.

--- test_settings.py
+++ test_settings.py
@@ -6,5 +6,11 @@ This means that the settings are as stripped down as possible.
 SECRET_KEY = 'secret'
 
 INSTALLED_APPS = (
+    # Third party apps
+    'django_nose',
+
+    # Custom apps
     'timetracker',
 )
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
