9aeef7ffa884d1fc975ec338503d4b95f0a55a7b
Chathan Driehuys <cdriehuys@gmail.com>
2016-08-25 14:36:19 -0400

    Removing default user for activities.
    The only reason there was a default user was to make migrations easier,
    but since the version is still less than 1.0, it's cleaner to just start
    from scratch on the migrations.

--- timetracker/models.py
+++ timetracker/models.py
@@ -4,22 +4,14 @@ These models are responsible for storing and representing the data that
 is manipulated within the app.
 """
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
 
 
-def _default_user():
-    """Get a default user to allow for migrations."""
-    return get_user_model().objects.create_user(username='DEFAULT_USER').id
-
-
 class Activity(models.Model):
     """An activity with a title, start time, and end time."""
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        default=_default_user)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=200)
     start_time = models.DateTimeField(default=timezone.now)
     end_time = models.DateTimeField(blank=True, null=True)
